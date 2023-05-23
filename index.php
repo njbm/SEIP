918d7f7bc4f8b0a43676e833ba312dbcc5887b08
Jaber <njb99m@gmail.com>
2023-05-23 13:20:47 +0600

    pondit project

---
+++ index.php
@@ -0,0 +1,4 @@
+<ul>
+    <li>Go to <a href="http://seip12.pondit.com/admin/layout_1/LTR/material/full/app_dashboard.php">Admin</a></li>
+    <li>Go to <a href="http://seip12.pondit.com/portal" >Portal</li>
+</ul>
